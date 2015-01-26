12f71cd1010a83352fe49014d698bb0e46910087
William Holt <wtholt27@gmail.com>
2015-01-25 20:48:16 -0500

    improved palindrome

--- palindromes.rb
+++ palindromes.rb
@@ -1,14 +1,12 @@
 require 'date'
 now = Date.today
-dates = (now<<12..now).map{ |day| day.strftime("%-m%-d%Y").squeeze(' ') }
+dates = (now<<60..now).map{ |day| day.strftime("%-m%-d%Y")}
   
 def palindrome?(dates)
   dates.each do |date|
     if date.reverse == date
       puts "#{date} is a Palindrome!"
-      break
     else
-      puts "#{date} is not a Palindrome!"
     end
   end
 end
